7eca42ce59f93d7391aab12cd2b7fad1ef837c99
Stephen Ramsay <rabrandom@gmail.com>
2019-12-11 14:24:19 +0000

    create student page and new page

--- students_controller.rb
+++ students_controller.rb
@@ -4,12 +4,19 @@ require_relative('models/student')
 require_relative('models/house')
 also_reload('./models/*')
 
-# index
-
+get '/students/?' do
+  @students = Student.all
+  erb(:index)
+end
 # show
-
+get '/students/:id' do
+  @student = Student.find(params[:id])
+  erb(:show)
+end
 # new
-
+get '/students/new/?' do
+  erb(:new)
+end
 # create
 
 # edit
